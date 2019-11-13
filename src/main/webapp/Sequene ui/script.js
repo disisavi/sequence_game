474fb9fe973066cba8e3aab29f4231c34a04bacb
Nagendra Prasad Prakash <prasadpgowda@gmail.com>
2019-11-13 18:02:49 -0500

    Random card generation

--- src/main/webapp/Sequene ui/script.js
+++ src/main/webapp/Sequene ui/script.js
@@ -1,7 +1,7 @@
 console.log('It works!');
 boardContent = new Array(100);
 var i=0;
-imagesArray = new Array(24);
+imagesArray = new Array(48);
 imagesArray[0] = "images/2C.jpg";
 imagesArray[1] = "images/2D.jpg";
 imagesArray[2] = "images/2H.jpg";
@@ -26,6 +26,30 @@ imagesArray[20] =  "images/7C.jpg";
 imagesArray[21] =  "images/7D.jpg";
 imagesArray[22] =  "images/7H.jpg";
 imagesArray[23] =  "images/7S.jpg";
+imagesArray[24] =  "images/8C.jpg";
+imagesArray[25] =  "images/8D.jpg";
+imagesArray[26] =  "images/8H.jpg";
+imagesArray[27] =  "images/8S.jpg";
+imagesArray[28] =  "images/9C.jpg";
+imagesArray[29] =  "images/9D.jpg";
+imagesArray[30] =  "images/9H.jpg";
+imagesArray[31] =  "images/9S.jpg";
+imagesArray[32] =  "images/10C.jpg";
+imagesArray[33] =  "images/10D.jpg";
+imagesArray[34] =  "images/10H.jpg";
+imagesArray[35] =  "images/10S.jpg";
+imagesArray[36] =  "images/AC.jpg";
+imagesArray[37] =  "images/AH.jpg";
+imagesArray[38] =  "images/ADi.jpg";
+imagesArray[39] =  "images/AS.jpg";
+imagesArray[40] =  "images/KC.jpg";
+imagesArray[41] =  "images/KD.jpg";
+imagesArray[42] =  "images/KH.jpg";
+imagesArray[43] =  "images/KS.jpg";
+imagesArray[44] =  "images/QC.jpg";
+imagesArray[45] =  "images/QD.jpg";
+imagesArray[46] =  "images/QH.jpg";
+imagesArray[47] =  "images/QS.jpg";
 
 
 
@@ -33,7 +57,6 @@ function loadImage()
 {
     var num = Math.floor(Math.random() * imagesArray.length);
     document.write('<img class="seq-img" src="'+imagesArray[num]+'"/>')
-   /* console.log(imagesArray[num]);*/
     boardContent[i] = imagesArray[num];
     i++;
    
