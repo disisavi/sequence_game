004164ea69fbf1d1742d4765496be04ca422d4ca
Abhijeet Mishra <disisavi@localhost.localdomain>
2019-11-25 01:37:21 -0500

    1. Added jetty support for runnig

--- src/main/java/edu/isa681/web/server/Params.java
+++ src/main/java/edu/isa681/web/server/Params.java
@@ -1,20 +1,27 @@
 package edu.isa681.web.server;
 
 public class Params {
-    private static Boolean isProd = true;
+    private static Boolean isProd = false;
+    private static Boolean isIDE = false;
 
     public static String getLoginParam() {
         if (isProd) {
             return "https://sequencegame.appspot.com/sq/login";
         }
-        return "http://localhost:8080/seq_war_exploded/sq/login";
+        if (isIDE) {
+            return "http://localhost:8080/seq_war_exploded/sq/login";
+        }
+        return "http://localhost:8080/sq/login";
     }
 
     public static String getBoardParam() {
         if (isProd) {
             return "https://sequencegame.appspot.com/sq/game/";
         }
-        return "http://localhost:8080/seq_war_exploded/sq/game/";
+        if (isIDE) {
+            return "http://localhost:8080/seq_war_exploded/sq/game/";
+        }
+        return "http://localhost:8080/sq/game/";
     }
 
 }
